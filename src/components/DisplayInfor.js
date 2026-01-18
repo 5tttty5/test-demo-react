dad1bea8c7d6328766d525f2be51d4e1297875ae
Nguyen Van Chi Thien <chithienfc123@gmail.com>
2026-01-18 22:31:28 +0700

    gg

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class DisplayInfor extends React.Component {
+
+    render() {
+        <div>
+            display inforgggg
+        </div>
+    }
+}
+
+
+export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,37 +2,21 @@
 // function component
 
 import React from "react";
+import UserInfor from './Userinfor';
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
-
-    state = {
-        name: "thien",
-        address: "hoi dan ti",
-        age: 21
-    };
-
-    handleClick = (event) => {
-        console.log("click me my buttoon");
-
-        // merge state => react class
-        this.setState({
-            name: "Erix",
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-        // this.setState({
-        //     age: Math.floor((Math.random() * 100) + 1)
-        // })
-    }
 
 
     //JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and age is {this.state.age}
-                <button onClick={this.handleClick}> lick me </button>
 
+                <UserInfor></UserInfor>
+                <DisplayInfor></DisplayInfor>
             </div>
+
         );
     }
 }
