da29a40b075c2a7fc9c8110c718f8b474af38975
Nguyen Van Chi Thien <chithienfc123@gmail.com>
2026-01-21 15:12:44 +0700

    video48

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
-import { Link } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 
 
@@ -11,18 +11,21 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <Header></Header>
-      <div>
-        test link
-        <div>
-          <button>
-            <Link to="/users"> go to user page</Link>
-          </button>
-          <button>
-            <Link to="/admins"> go to admin page</Link>
-          </button>
+      <div className='header-container'>
+        <Header></Header>
+
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        <div className='app-content'>
+          <Outlet></Outlet>
         </div>
       </div>
+
+
+
     </div>
   );
 }
