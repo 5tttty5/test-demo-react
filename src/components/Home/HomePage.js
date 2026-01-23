429ef045ede1774907ed588be6ffb4d5dc807e5d
Nguyen Van Chi Thien <chithienfc123@gmail.com>
2026-01-23 15:24:31 +0700

    video54

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -9,6 +9,17 @@ const HomePage = (props) => {
                     type="video/mp4"
                 ></source>
             </video>
+
+            <div className='homepage-content'>
+                <div className='title-1'>heeeheeee to ask</div>
+                <div className='title-2'>you dontttttt
+                    tttt
+                    ttttttt
+                </div>
+                <div className='title-3'>
+                    <button>get's     gggggg</button>
+                </div>
+            </div>
         </div>
     )
 }
