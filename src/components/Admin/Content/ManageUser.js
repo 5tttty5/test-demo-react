1b05593568a70bb9d736889e77887f088b4f88fe
Nguyen Van Chi Thien <chithienfc123@gmail.com>
2026-01-26 15:50:52 +0700

    video61

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import ModelCreateUser from './ModelCreateUser';
 import './ManageUser.scss';
 import { FcPlus } from 'react-icons/fc';
 import TableUser from './TableUser';
-
+import { useEffect, useState } from "react";
+import { getAllUsers } from '../../../services/apiService'
 
 const ManageUser = (props) => {
 
     const [showModelCreateUser, setShowModelCreateUser] = useState(false);
+    const [listUsers, setListUsers] = useState([])
+    useEffect(() => {
+        fetchListUsers();
+    }, []);
+
+    const fetchListUsers = async () => {
+        let res = await getAllUsers();
+        console.log(res);
+        if (res.EC === 0) {
+            setListUsers(res.DT);
+        }
+    }
+
     return (
         <div className="manage-user-container">
             <div className="title">
@@ -21,11 +35,12 @@ const ManageUser = (props) => {
                     <button className='btn btn-primary' onClick={() => setShowModelCreateUser(true)}><FcPlus></FcPlus> Add new users</button>
                 </div>
                 <div>
-                    <TableUser />
+                    <TableUser listUsers={listUsers} />
 
                 </div>
                 <ModelCreateUser show={showModelCreateUser}
                     setShow={setShowModelCreateUser}
+                    fetchListUsers={fetchListUsers}
                 ></ModelCreateUser>
             </div>
         </div>
